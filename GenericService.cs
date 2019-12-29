5b1f452e39aca2e90f11bc367553631945b23c3d
jabu <jabu@rubiblue.co.za>
2019-12-29 13:26:19 +0200

    first commit

---
+++ GenericService.cs
@@ -0,0 +1,97 @@
+﻿using LuciaTech.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuciaTech.Service
+{
+    public abstract class GenericService<T> : IGenericService<T>
+        where T : class, new()
+    {
+        protected IGenericRepository<T> EntityRepository { get; }
+        protected GenericService(IGenericRepository<T> entityRepository)
+        {
+
+            EntityRepository = entityRepository;
+
+        }
+
+        public async Task<int> DeleteAsync(T entity)
+        {
+            return await EntityRepository.DeleteAsync(entity);
+        }
+
+        public async Task<int> DeleteRange(List<T> entities)
+        {
+            return await EntityRepository.DeleteRangeAsync(entities);
+        }
+
+        public async Task<ICollection<T>> GetAllAsync()
+        {
+            return await EntityRepository.Query().ToListAsync();
+        }
+
+        //public async Task<ICollection<T>> GetAllAsync()
+        //{
+        //    return await EntityRepository.Query().ToListAsync();
+        //}
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await EntityRepository.GetAsync(id);
+        }
+
+        public async Task<T> InsertAsync(T entity)
+        {
+            return await EntityRepository.InsertAsync(entity);
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            return await EntityRepository.UpdateAsync(entity);
+        }
+
+        public async Task<ICollection<T>> UpdateRangeAsync(ICollection<T> entities)
+        {
+            return await EntityRepository.UpdateRangeAsync(entities);
+        }
+
+        public async Task<int> DeleteByIdsRange(ICollection<int> ids)
+        {
+            return await EntityRepository.DeleteByIdsRangeAsync(ids);
+        }
+
+        public async Task<T> GetByIdAsync(string id)
+        {
+
+            return await EntityRepository.GetAsync(id);
+        }
+
+        public async Task<ICollection<T>> GetByIdsAsync(ICollection<int> ids)
+        {
+            return await EntityRepository.GetByIdsAsync(ids);
+        }
+
+        public async Task<ICollection<T>> GetByIdsAsync(ICollection<string> ids)
+        {
+            return await EntityRepository.GetByIdsAsync(ids);
+        }
+
+        public async Task<ICollection<T>> InsertRangeAsync(ICollection<T> entity)
+        {
+            return await EntityRepository.InsertRangeAsync(entity);
+        }
+
+        public async Task<int> DeleteRangeAsync(ICollection<T> entities)
+        {
+            return await EntityRepository.DeleteRangeAsync(entities);
+        }
+
+        public async Task<int> DeleteByIdsRange(ICollection<string> ids)
+        {
+            return await EntityRepository.DeleteByIdsRangeAsync(ids);
+        }
+    }
+}
---
+++ IGenericService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuciaTech.Service
+{
+    public interface IGenericService<T>
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<T> GetByIdAsync(string id);
+        Task<ICollection<T>> GetByIdsAsync(ICollection<int> ids);
+        Task<ICollection<T>> GetByIdsAsync(ICollection<string> ids);
+        Task<T> InsertAsync(T entity);
+        Task<ICollection<T>> InsertRangeAsync(ICollection<T> entity);
+        Task<T> UpdateAsync(T entity);
+        Task<int> DeleteAsync(T entity);
+        Task<ICollection<T>> UpdateRangeAsync(ICollection<T> entities);
+        Task<int> DeleteRangeAsync(ICollection<T> entities);
+        Task<int> DeleteByIdsRange(ICollection<int> ids);
+        Task<int> DeleteByIdsRange(ICollection<string> ids);
+        Task<ICollection<T>> GetAllAsync();
+    }
+}
